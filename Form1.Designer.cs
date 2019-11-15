d35c338b97e911015caa89939d98bc1f6eb9b85d
levdjepko <levdjepko@gmail.com>
2019-11-15 14:48:58 -0800

    Added files

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Hello2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.initialBox = new System.Windows.Forms.TextBox();
+            this.additionalBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.totalTimeBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(63, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Initial investment";
+            // 
+            // initialBox
+            // 
+            this.initialBox.Location = new System.Drawing.Point(212, 45);
+            this.initialBox.Name = "initialBox";
+            this.initialBox.Size = new System.Drawing.Size(177, 22);
+            this.initialBox.TabIndex = 2;
+            // 
+            // additionalBox
+            // 
+            this.additionalBox.Location = new System.Drawing.Point(212, 91);
+            this.additionalBox.Name = "additionalBox";
+            this.additionalBox.Size = new System.Drawing.Size(177, 22);
+            this.additionalBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Additional investment a year";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Frequency";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Items.AddRange(new object[] {
+            "Yearly",
+            "Monthly"});
+            this.listBox1.Location = new System.Drawing.Point(212, 137);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(61, 34);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // totalTimeBox
+            // 
+            this.totalTimeBox.Location = new System.Drawing.Point(212, 194);
+            this.totalTimeBox.Name = "totalTimeBox";
+            this.totalTimeBox.Size = new System.Drawing.Size(177, 22);
+            this.totalTimeBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Total time in years";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Total amount";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "4% Rule";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(171, 241);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "...";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(171, 272);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 16);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "...";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(293, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 27);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 405);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.totalTimeBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.additionalBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.initialBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Lucida Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Easy investment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox initialBox;
+        private System.Windows.Forms.TextBox additionalBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox totalTimeBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DefaultZeros();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalculateInvestment();
+        }
+
+        public void DefaultZeros()
+        {
+            initialBox.Text = "0";
+            additionalBox.Text = "0";
+            totalTimeBox.Text = "0";
+            listBox1.SelectedIndex = listBox1.TopIndex;
+        }
+        public void CalculateInvestment()
+        {
+
+            double total = 0.0;
+            try { total = double.Parse(initialBox.Text); }
+            catch { total = 0.0; MessageBox.Show("No initial value"); }
+
+
+            try
+            {
+                //increment by using the amount of money from TextBox
+                if (listBox1.SelectedIndex == listBox1.TopIndex)
+                {
+                    for (int i = 1; i <= int.Parse(totalTimeBox.Text); i++)
+                    {
+                        total = total * 1.07 + double.Parse(additionalBox.Text);
+                    }
+                }
+
+                //for monthly deposits/adjustments
+                if (listBox1.SelectedIndex != listBox1.TopIndex)
+                {
+                    for (int i = 1; i <= int.Parse(totalTimeBox.Text) * 12; i++)
+                    {
+                        total = total * 1.0058333 + double.Parse(additionalBox.Text);
+                    }
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("You forgot to add some values in the fields");
+            }
+
+            //Display total value and the one using 4% rule
+            void ShowResult()
+            {
+                label7.Text = "$ " + total.ToString("0.00");
+                label8.Text = "$ " + (total * 0.04 / 12).ToString("0.00");
+            }
+
+            ShowResult();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            //nothing
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != listBox1.TopIndex)
+            {
+                label2.Text = "Additional investment a month";
+            }
+
+            if (listBox1.SelectedIndex == listBox1.TopIndex)
+            {
+                label2.Text = "Additional investment a year";
+            }
+        }
+    }
+}
